94ab9198977bf3ed5194a8ec3c457ccc8776281a
Kunsh KUVAM <kkuvam@gmail.com>
2022-06-05 23:27:12 +0530

    Add the et spider

--- scrape/news/spiders/et.py
+++ scrape/news/spiders/et.py
@@ -1,3 +1,4 @@
+from urllib import response
 import scrapy
 import calendar
 
@@ -22,11 +23,26 @@ class ETSpider(scrapy.Spider):
                     start = start + 1
                     
         for url in day_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_url)
 
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+
+    def parse_url(self, response):
+        # Select <ul class="content"> and extract hrefs
+        links = response.xpath('//ul[@class="content"]//a/@href').getall()
+        for link in links:
+            yield response.follow(link, callback=self.parse_item)
+    
+    def parse_item(self, response):
+        # Process the individual news item
+        # Select the <div class="artText"> from the page
+        art_text = response.xpath('//div[@class="artText"]//text()').getall()
+        if art_text:
+            # Join and clean the text similar to get_text(separator='\n', strip=True)
+            clean_text = '\n'.join([line.strip() for line in art_text if line.strip()])
+            self.logger.info(f"Extracted content from {response.url}")
+        
+            # Or yield it as an item
+            yield {
+                'url': response.url,
+                'text': clean_text
+            }
